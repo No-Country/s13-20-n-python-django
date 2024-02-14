bd6795a5f49236ed76da9ea9344ff188f76640c4
suspiciousRaccoon <spablocabral@gmail.com>
2024-02-13 21:06:08 -0300

    feat(core/settings): add email settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -121,3 +121,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email settings
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PORT = int(os.getenv("EMAIL_HOST_PORT", 25))
+EMAIL_USE_TLS = True if os.getenv("EMAIL_USE_TLS") == "True" else False
+EMAIL_USE_SSL = True if os.getenv("EMAIL_USE_SSL") == "True" else False
+EMAIL_TIMEOUT = (
+    None if os.getenv("EMAIL_TIMEOUT") == "None" else int(os.getenv("EMAIL_TIMEOUT"))
+)  # this will fail if anything but None or an integer is submitted. oh well
