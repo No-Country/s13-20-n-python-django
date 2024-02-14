64280b55cc1d0a8e3ce4faadb0a8b30da1876173
suspiciousRaccoon <spablocabral@gmail.com>
2024-02-13 21:19:59 -0300

    fix(core/settings.py): typo

--- app/core/settings.py
+++ app/core/settings.py
@@ -25,7 +25,8 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KET")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
