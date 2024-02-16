cb92c776e38f78971172cee3931332fa0ed64c15
Nahuel <nahuel.94.arrieta@gmail.com>
2024-02-16 10:57:56 -0300

    feat(settings): add app accounts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,11 +44,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    "task",
-
+    "accounts",
+    "tasks",
     "rest_framework",
-
 ]
 
 MIDDLEWARE = [
