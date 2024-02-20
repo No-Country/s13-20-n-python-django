f2b87fcafeee4b0452715a89942cd85c24d52cca
Nahuel <nahuel.94.arrieta@gmail.com>
2024-02-19 21:22:16 -0300

    feat(models/task):add related_name to models

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -4,18 +4,22 @@ from accounts.models import User
 
 
 class Project(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="user_project", null=True)
     name = models.CharField(max_length=255)
 
 
 class Board(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="project_board"
+    )
     name = models.CharField(max_length=255, blank=True)
     description = models.TextField(max_length=255, blank=True)
 
 
 class List(models.Model):
-    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    board = models.ForeignKey(
+        Board, on_delete=models.CASCADE, related_name="board_list"
+    )
     title = models.CharField(max_length=255, blank=True)
     order = models.IntegerField()
 
@@ -27,8 +31,10 @@ class Task(models.Model):
         DELEGATE = 3, gettext_noop("DELEGATE")
         URGENT = 4, gettext_noop("URGENT")
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    list = models.ForeignKey(List, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="user_task", null=True
+    )
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="list_task",null=True)
     order = models.IntegerField(default=1)
     name = models.CharField(max_length=255, blank=True)
     description = models.TextField(max_length=255, blank=True)
@@ -41,15 +47,20 @@ class Task(models.Model):
 
 
 class File(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, blank=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="task_file"blank=True)
     file = models.ImageField(blank=True)
 
 
 class Milestone(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="project_milestone"
+    )
     complete = models.BooleanField(default=False)
     tasks = models.ManyToManyField(
-        Task, through="MilestoneTask", through_fields=("milestone", "task")
+        Task,
+        through="MilestoneTask",
+        related_name="task_milestone",
+        through_fields=("milestone", "task"),
     )
 
 
@@ -59,8 +70,12 @@ class MilestoneTask(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="user_comment", null=True
+    )
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name="task_comment", blank=True
+    )
     content = models.CharField(max_length=255, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
 
