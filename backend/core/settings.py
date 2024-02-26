482f3196f50cc03cae0397bcbfec05cac4faeb9f
Nahuel <nahuel.94.arrieta@gmail.com>
2024-02-26 11:07:40 -0300

    feat(core/settings): Add pagination global

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -154,6 +154,8 @@ AUTH_USER_MODEL = "accounts.User"
 
 # rest_framwork settings
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
