81cbaafee90cce6de90f480f1cc5d9f5aac03a32
suspiciousRaccoon <spablocabral@gmail.com>
2024-02-26 23:54:28 -0300

    feat(services): add slice for accounts

---
+++ frontend/src/services/accountSlice.js
@@ -0,0 +1,43 @@
+import { apiSlice } from "./apiSlice.js";
+
+// For what should go inside userData check the documentation
+const extendedUserApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => "api-user/",
+    }),
+    getUser: builder.query({
+      query: (id) => ({
+        url: `api-user/${id}/`,
+      }),
+    }),
+    createNewUser: builder.mutation({
+      query: (userData) => ({
+        url: "api-user/",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: (userData) => ({
+        url: `/user/${userData.id}/`,
+        method: "PUT",
+        body: userData,
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `user/${id}/`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  useGetUserQuery,
+  useCreateNewUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = extendedUserApiSlice;
