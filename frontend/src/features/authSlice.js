7cb4c0867971df8d718c656905ab0c01c232c835
suspiciousRaccoon <spablocabral@gmail.com>
2024-02-27 00:01:25 -0300

    feat(features): add authSlice

---
+++ frontend/src/features/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { username: null, refresh_token: null, token: null },
+  reducers: {
+    setCredentials: (
+      state,
+      { payload: { refresh_token, token, username } }
+    ) => {
+      state.refresh_token = refresh_token;
+      state.token = token;
+      state.username = username;
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
