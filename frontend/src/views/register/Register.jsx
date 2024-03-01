e5ef3510dc748ebc77b3585b2d9b25f4f8c81da9
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-01 00:01:00 -0300

    feat(views/register): add username field

--- frontend/src/views/register/Register.jsx
+++ frontend/src/views/register/Register.jsx
@@ -11,6 +11,7 @@ const Register = () => {
   const [firstName, setFirstName] = useState("");
   const [secondName, setSecondName] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [username, setUsername] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [
@@ -40,6 +41,7 @@ const Register = () => {
     createUser({
       email,
       password,
+      username,
       first_name: firstName,
       last_name: secondName,
       image_url: imageUrl,
@@ -67,8 +69,7 @@ const Register = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
+            className="w-4 h-4 opacity-70">
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
@@ -89,8 +90,7 @@ const Register = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
+            className="w-4 h-4 opacity-70">
             <path
               fillRule="evenodd"
               d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
@@ -115,8 +115,7 @@ const Register = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
+            className="w-4 h-4 opacity-70">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
           <label htmlFor="first-name"></label>
@@ -136,8 +135,7 @@ const Register = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
+            className="w-4 h-4 opacity-70">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
           <label htmlFor="second-name"></label>
@@ -151,6 +149,27 @@ const Register = () => {
             onChange={(e) => setSecondName(e.target.value)}
           />
         </label>
+        {/* username */}
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+          </svg>
+          <label htmlFor="second-name"></label>
+          <input
+            name="username"
+            type="text"
+            placeholder="Username"
+            className="grow"
+            value={username}
+            // minLength={14}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
         {/* image url */}
         <label className="input input-bordered flex items-center gap-2">
           <svg
@@ -159,8 +178,7 @@ const Register = () => {
             viewBox="0 0 24 24"
             strokeWidth={2.5}
             stroke="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
+            className="w-4 h-4 opacity-70">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
