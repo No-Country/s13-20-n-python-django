9a8548275be541166274bc3819bd92bf7b34d1b9
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-01 18:55:31 -0300

    fix(tasks/urls.py): missing mehod

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 urlpatterns = [
     path("projects/", ProjectListView.as_view(), name="project-list"),
-    path(f"projects/<id>", ProjectRetrieveView, name="project-retrieve"),
+    path("projects/<id>", ProjectRetrieveView.as_view(), name="project-retrieve"),
     path("projects/update/<id>", ProjectUpdateView.as_view(), name="project-update"),
     path("projects/create/", ProjectCreateView.as_view(), name="project-list-create"),
     path(
