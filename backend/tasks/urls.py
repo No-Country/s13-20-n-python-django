d245cd14fab7cba44286484f019f5d3424aa585a
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-01 18:49:11 -0300

    refactor(tasks/urls.py): modify urls for new project views

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-from .views import ProjectListCreate
+from .views import (
+    ProjectCreateView,
+    ProjectDeleteView,
+    ProjectListView,
+    ProjectRetrieveView,
+    ProjectUpdateView,
+)
 
 urlpatterns = [
-    # path('projects/', ProjectList.as_view({'get': 'list', 'post': 'create'}), name='project-list'),
-    # # para filtrar actualizar y borrar un proyecto necesitas mandar en el url el id del proyecto
-    # path('projects/<int:pk>/', ProjectDetail.as_view({'get': 'retrieve', 'put': 'update','delete':'destroy','post':'create'}), name='project-detail'),
-    # # para listar todoas las boards o crear una nueva board necesitas mandarle
-    # # el id del proyecto al cual quieres o pertece la board
-    # path('board/<int:pk>/', BoardProject.as_view({'get': 'list', 'post': 'create'}), name='board-task'),
-    # #para listas todas listas que pertecen a un proyecto o crear una nueva
-    # # debes mandar el id de la board a la cual pertece la lista
-    # path('list-tasks/<int:pk>/', ListProject.as_view({'get': 'list', 'post': 'create'}), name='list-task'),
-    # path('<int:pk>/', TaskProject.as_view({'get': 'list', 'post': 'create'}), name='task'),
-    path("projects/", ProjectListCreate.as_view(), name="project-list-create")
+    path("projects/", ProjectListView.as_view(), name="project-list"),
+    path(f"projects/<id>", ProjectRetrieveView, name="project-retrieve"),
+    path("projects/update/<id>", ProjectUpdateView.as_view(), name="project-update"),
+    path("projects/create/", ProjectCreateView.as_view(), name="project-list-create"),
+    path(
+        "projects/delete/<id>", ProjectDeleteView.as_view(), name="project-list-create"
+    ),
 ]
