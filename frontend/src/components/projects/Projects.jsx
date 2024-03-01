8c0a35164ad6162b361802be77f752d9ab0edcaf
suspiciousRaccoon <suspiciousRaccoon@users.noreply.github.com>
2024-03-01 02:44:56 +0000

    Prettified Code!

--- frontend/src/components/projects/Projects.jsx
+++ frontend/src/components/projects/Projects.jsx
@@ -1,19 +1,18 @@
-
-import ProjectItem from './ProjectItem';
+import ProjectItem from "./ProjectItem";
 
 function ProjectsComp() {
-    return (
-        <div className='p-4 w-full overflow-auto'>
-            <h1 className="text-2xl font-bold">Projects</h1>
-            {/* project list */}
-            <div className=''>
-                <div className="flex flex-col items-center md:flex-row md:flex-wrap gap-6 py-6">
-                    <ProjectItem />
-                    <ProjectItem />
-                </div>
-            </div>
+  return (
+    <div className="p-4 w-full overflow-auto">
+      <h1 className="text-2xl font-bold">Projects</h1>
+      {/* project list */}
+      <div className="">
+        <div className="flex flex-col items-center md:flex-row md:flex-wrap gap-6 py-6">
+          <ProjectItem />
+          <ProjectItem />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ProjectsComp
+export default ProjectsComp;
--- frontend/src/views/projects/Projects.jsx
+++ frontend/src/views/projects/Projects.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ProjectsComp  from "../../components/projects/Projects"
+import ProjectsComp from "../../components/projects/Projects";
 import Sidebar from "../../components/layout/Sidebar";
 
 const Projects = () => {
-  return(
+  return (
     <>
       <Sidebar />
       <ProjectsComp />
     </>
-  )
+  );
 };
 
 export default Projects;
