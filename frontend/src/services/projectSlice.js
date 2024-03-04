324de01271e5dd0ac1b747c033ecd3bbb015da66
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-04 20:24:15 -0300

    feat(services): add project Slice

---
+++ frontend/src/services/projectSlice.js
@@ -0,0 +1,43 @@
+import { apiSlice } from "./apiSlice.js";
+
+// For what should go inside projectData check the documentation
+const extendedProjectApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProjects: builder.query({
+      query: () => "tasks/projects",
+    }),
+    getProject: builder.query({
+      query: (ProjectId) => ({
+        url: `tasks/projects/${ProjectId}/`,
+      }),
+    }),
+    createNewProject: builder.mutation({
+      query: (projectData) => ({
+        url: "tasks/projects/",
+        method: "POST",
+        body: projectData,
+      }),
+    }),
+    updateProject: builder.mutation({
+      query: (projectData) => ({
+        url: `tasks/projects/${projectData.id}/`,
+        method: "PUT",
+        body: projectData,
+      }),
+    }),
+    deleteProject: builder.mutation({
+      query: (projectId) => ({
+        url: `tasks/projects/${projectId}/`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetProjectQuery,
+  useGetProjectsQuery,
+  useCreateNewProjectMutation,
+  useUpdateProjectMutation,
+  useDeleteProjectMutation,
+} = extendedProjectApiSlice;
