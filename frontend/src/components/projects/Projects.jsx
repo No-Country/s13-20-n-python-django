b51c1989e60d92121b700ea1d87ede0db1513f63
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-05 23:33:30 -0300

    chore: formatting

--- frontend/src/components/projects/Projects.jsx
+++ frontend/src/components/projects/Projects.jsx
@@ -1,17 +1,16 @@
 import ProjectItem from "./ProjectItem";
-import projectData from '../../data/projects.json'
+import projectData from "../../data/projects.json";
 
 function Projects() {
   return (
-
-    <div className="p-4 w-full overflow-auto">
-      <h1 className="text-2xl font-bold">My Projects</h1>
+    <div className='p-4 w-full overflow-auto'>
+      <h1 className='text-2xl font-bold'>My Projects</h1>
       {/* project list */}
-      <div className="">
-        <div className="flex flex-col items-center md:flex-row md:flex-wrap gap-6 py-6">
+      <div className=''>
+        <div className='flex flex-col items-center md:flex-row md:flex-wrap gap-6 py-6'>
           {projectData.map((project, index) => (
             <ProjectItem key={index} project={project} board={project} />
-        ))}
+          ))}
         </div>
       </div>
     </div>
