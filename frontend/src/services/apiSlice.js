0255409a612048392aa64f293ac82c5b46b8c85a
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-06 00:47:22 -0300

    fix(apiSlice): wrong location for prepareHeaders

--- frontend/src/services/apiSlice.js
+++ frontend/src/services/apiSlice.js
@@ -5,15 +5,15 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     // Change to production url later
     baseUrl: "http://localhost:8000/api/",
+    prepareHeaders: async (headers, { getState }) => {
+      headers.set("Content-type", "application/json");
+      const token = getState().auth.access;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
   }),
-  prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token;
-
-    if (token) {
-      headers.set("authorization", `Bearer ${token}`);
-    }
-
-    return headers;
-  },
   endpoints: () => ({}), // needed, else we can't inject endpoints
 });
