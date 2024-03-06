69bd5f0b6c2e9ca1d4c08e846528bfe7ce3c2037
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-06 00:58:39 -0300

    feat(components/projects): use endpoint data isntead of mock

--- frontend/src/components/projects/Projects.jsx
+++ frontend/src/components/projects/Projects.jsx
@@ -1,16 +1,23 @@
 import ProjectItem from "./ProjectItem";
-import projectData from "../../data/projects.json";
+import { useGetProjectsQuery } from "../../services/projectSlice";
 
 function Projects() {
+  const { data, isLoading, isError } = useGetProjectsQuery();
+  console.log(data, isError);
+
   return (
     <div className='p-4 w-full overflow-auto'>
       <h1 className='text-2xl font-bold'>My Projects</h1>
       {/* project list */}
       <div className=''>
         <div className='flex flex-col items-center md:flex-row md:flex-wrap gap-6 py-6'>
-          {projectData.map((project, index) => (
-            <ProjectItem key={index} project={project} board={project} />
-          ))}
+          {isLoading || isError ? (
+            <div>loading</div>
+          ) : (
+            data.map((project, index) => (
+              <ProjectItem key={index} project={project} board={project} />
+            ))
+          )}
         </div>
       </div>
     </div>
