eb8830270b89b72bd03a44576ae5eeceb1ab9183
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-07 05:19:53 -0300

    chore: remove import

--- frontend/src/components/projects/Project.jsx
+++ frontend/src/components/projects/Project.jsx
@@ -1,31 +1,30 @@
 import { PropTypes } from "prop-types";
 import ProjectItem from "./ProjectItem";
-import AddProject from "./AddProject";
 
 function Project({ project }) {
-	if (!project) {
-		// Added guard clause
-		return <div>No project provided</div>; // Added a simple fallback
-	}
+  if (!project) {
+    // Added guard clause
+    return <div>No project provided</div>; // Added a simple fallback
+  }
 
-	return (
-		<>
-			<ProjectItem project={project} />
-		</>
-	);
+  return (
+    <>
+      <ProjectItem project={project} />
+    </>
+  );
 }
 
 Project.propTypes = {
-	project: PropTypes.shape({
-		name: PropTypes.string.isRequired,
-		id: PropTypes.number.isRequired,
-		project_board: PropTypes.arrayOf(
-			PropTypes.shape({
-				id: PropTypes.number.isRequired,
-				name: PropTypes.string.isRequired,
-			})
-		),
-	}),
+  project: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    project_board: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+      })
+    ),
+  }),
 };
 
 export default Project;
