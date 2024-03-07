34fa6550fc022d489a230ad36508e7f88f2a239f
suspiciousRaccoon <spablocabral@gmail.com>
2024-03-07 04:27:46 -0300

    fix: demons

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -23,21 +23,25 @@ from .views import (
 
 urlpatterns = [
     path("projects/", ProjectListView.as_view(), name="project-list"),
-    path("projects/<pk>/", ProjectRetrieveView.as_view(), name="project-retrieve"),
-    path("projects/update/<pk>/", ProjectUpdateView.as_view(), name="project-update"),
+    path("projects/<int:pk>/", ProjectRetrieveView.as_view(), name="project-retrieve"),
+    path(
+        "projects/update/<int:pk>/", ProjectUpdateView.as_view(), name="project-update"
+    ),
     path("projects/create/", ProjectCreateView.as_view(), name="project-create"),
-    path("projects/delete/<pk>/", ProjectDeleteView.as_view(), name="project-delete"),
-    path("boards/", BoardListView.as_view(), name="board-list"),  # won't be used
-    path("boards/<pk>/", BoardRetrieveView.as_view(), name="board-retrieve"),
-    path("boards/update/<pk>/", BoardUpdateView.as_view(), name="board-update"),
-    path("boards/create/", BoardCreateView.as_view(), name="board-update"),
-    path("boards/delete/<pk>/", BoardDeleteView.as_view(), name="board-destroy"),
-    path("lists/<pk>/", ListRetrieveView.as_view(), name="list-retrieve"),
-    path("lists/update/<pk>/", ListUpdateView.as_view(), name="list-update"),
+    path(
+        "projects/delete/<int:pk>/", ProjectDeleteView.as_view(), name="project-delete"
+    ),
+    path("boards/", BoardListView.as_view(), name="board-list"),
+    path("boards/<int:pk>/", BoardRetrieveView.as_view(), name="board-retrieve"),
+    path("boards/update/<int:pk>/", BoardUpdateView.as_view(), name="board-update"),
+    path("boards/create/", BoardCreateView.as_view(), name="board-create"),
+    path("boards/delete/<int:pk>/", BoardDeleteView.as_view(), name="board-destroy"),
+    path("lists/<int:pk>/", ListRetrieveView.as_view(), name="list-retrieve"),
+    path("lists/update/<int:pk>/", ListUpdateView.as_view(), name="list-update"),
     path("lists/create/", ListCreateView.as_view(), name="list-create"),
-    path("lists/delete/<pk>/", ListDeleteView.as_view(), name="list-delete"),
-    path("tasks/<pk>/", TaskRetrieveView.as_view(), name="task-retrieve"),
-    path("tasks/update/<pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("lists/delete/<int:pk>/", ListDeleteView.as_view(), name="list-delete"),
+    path("tasks/<int:pk>/", TaskRetrieveView.as_view(), name="task-retrieve"),
+    path("tasks/update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/delete/<pk>/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
 ]
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,38 +1,41 @@
-from rest_framework.permissions import IsAuthenticated
+from accounts.models import User
 from django.db.models import Q
-from .permissions import (
-    BoardUserIsProjectOwnerOrReadOnly,
-    ProjectIsOwnerOrReadOnly,
-    ListUserIsProjectMemberOrReadOnly,
-    TaskUserIsProjectMemberOrReadOnly,
-)
-from .models import Project, List, Board, Task
-from .serializers import (
-    ListSerializer,
-    ProjectSerializer,
-    DetailProjectSerializer,
-    BoardSerializer,
-    DetailBoardSerializer,
-    TaskSerializer,
-)
 from rest_framework.generics import (
-    ListAPIView,
     CreateAPIView,
     DestroyAPIView,
+    ListAPIView,
     RetrieveAPIView,
     UpdateAPIView,
 )
-from accounts.models import User
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Board, List, Project, Task
+from .permissions import (
+    BoardUserIsProjectOwnerOrReadOnly,
+    ListUserIsProjectMemberOrReadOnly,
+    ProjectIsOwnerOrReadOnly,
+    TaskUserIsProjectMemberOrReadOnly,
+)
+from .serializers import (
+    BoardSerializer,
+    DetailBoardSerializer,
+    DetailProjectSerializer,
+    ListSerializer,
+    ProjectSerializer,
+    TaskSerializer,
+)
 
 
 class ProjectListView(ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     user_field = "owner"
-    
+
     def get_queryset(self):
-        return super().get_queryset().filter(
-            Q(owner=self.request.user) | Q(member=self.request.user)
+        return (
+            super()
+            .get_queryset()
+            .filter(Q(owner=self.request.user) | Q(member=self.request.user))
         )
 
 
@@ -55,7 +58,6 @@ class ProjectDeleteView(DestroyAPIView):
 class ProjectRetrieveView(RetrieveAPIView):
     queryset = Project.objects.all()
     serializer_class = DetailProjectSerializer
- 
 
 
 class ProjectUpdateView(UpdateAPIView):
@@ -77,7 +79,7 @@ class BoardListView(ListAPIView):
 
 
 class BoardCreateView(CreateAPIView):
-    queryset = Board.objects.select_related("project")
+    queryset = Board.objects.all()
     serializer_class = BoardSerializer
     permission_classes = [IsAuthenticated, BoardUserIsProjectOwnerOrReadOnly]
 
